2c5c75376926b1b6cd1bbc00aae15c3e307a980c
Davis W. Frank <dwfrank@gmail.com>
2024-12-02 13:38:40 -0800

    Day 2, Part 2 (with some refactoring)

--- lib/aoc2024/day2.rb
+++ lib/aoc2024/day2.rb
@@ -8,25 +8,49 @@ module Aoc2024
         .map { |a| a.map(&:to_i) }
     end
 
-    class Puzzle1
+    class Puzzle
       def initialize(lines:)
         @lines = lines
       end
 
       def solution
-        @lines.filter_map do |line|
-          if line != line.sort && line != line.sort.reverse
-            false
-          else
-            line[0..-2].each_with_index
-              .map { |level, i| (level - line[i + 1]).abs }
-              .all? { |n| [1, 2, 3].include?(n) }
-          end
-        end.size
+        @lines.filter_map { |line| safe?(line) }.size
+      end
+
+      def solution_with_dampers
+        @lines.filter_map { |line| safe?(line) || safe_with_dampener?(line) }.size
       end
 
       def self.from(filename)
         new(lines: Day2.parse(filename))
+      end
+
+      private
+
+      def safe?(line)
+        levels_all_ascend_or_descend(line) && all_levels_within_tolerances?(line)
+      end
+
+      def all_levels_within_tolerances?(line)
+        line[0..-2].each_with_index
+          .map { |level, i| (level - line[i + 1]).abs }
+          .all? { |n| [1, 2, 3].include?(n) }
+      end
+
+      def levels_all_ascend_or_descend(line)
+        line == line.sort || line == line.sort.reverse
+      end
+
+      def safe_with_dampener?(line)
+        remove = 0
+        while remove <= line.size - 1
+          without_level = line.dup.tap { |l| l.delete_at(remove) }
+          if safe?(without_level)
+            return true
+          end
+          remove += 1
+        end
+        false
       end
     end
   end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -2,18 +2,34 @@
 
 require "rspec"
 module Aoc2024::Day2
-  RSpec.describe Puzzle1 do
+  RSpec.describe Puzzle do
     context "with test data" do
       it "solves correctly" do
-        puzzle = Puzzle1.from("spec/data/input_0201_test.txt")
+        puzzle = Puzzle.from("spec/data/input_0201_test.txt")
         expect(puzzle.solution).to eq(2)
       end
     end
 
     context "with real data" do
       it "solves correctly" do
-        puzzle = Puzzle1.from("spec/data/input_0201.txt")
+        puzzle = Puzzle.from("spec/data/input_0201.txt")
         expect(puzzle.solution).to eq(490)
+      end
+    end
+  end
+
+  RSpec.describe Puzzle do
+    context "with test data" do
+      it "solves correctly" do
+        puzzle = Puzzle.from("spec/data/input_0201_test.txt")
+        expect(puzzle.solution_with_dampers).to eq(4)
+      end
+    end
+
+    context "with real data" do
+      it "solves correctly" do
+        puzzle = Puzzle.from("spec/data/input_0201.txt")
+        expect(puzzle.solution_with_dampers).to eq(536)
       end
     end
   end
