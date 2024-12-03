6ea04206f9174848c091dc89b65effbed110587d
Davis W. Frank <dwfrank@gmail.com>
2024-12-03 11:43:39 -0800

    Solves Day03, Part 2

--- lib/aoc2024/day3.rb
+++ lib/aoc2024/day3.rb
@@ -2,19 +2,36 @@
 
 module Aoc2024
   module Day3
-    def self.parse(filename)
+    def self.raw_parse(filename)
       File.readlines(filename, chomp: true)
-        .map { |line| line.scan(/mul(\(\d+,\d+\))?/) }
-        .flatten
-        .map do |mult|
+          .map { |line| line.scan(/mul(\(\d+,\d+\))?/) }
+          .flatten
+          .map do |mult|
+        /(?<a>\d+),(?<b>\d+)/ =~ mult
+        [a.to_i, b.to_i]
+      end
+    end
+
+    def self.parse_with_toggle(filename)
+      input = File.read(filename, chomp: true)
+      input.split("do")
+          .flatten
+          .select { |segment| !(segment =~ /^n't/) }
+          .map { |segment| segment.scan(/mul(\(\d+,\d+\))?/) }
+          .flatten
+          .map do |mult|
         /(?<a>\d+),(?<b>\d+)/ =~ mult
         [a.to_i, b.to_i]
       end
     end
 
     class Puzzle
-      def self.from(filename)
-        new(pairs: Day3.parse(filename))
+      def self.from_raw_parsing(filename)
+        new(pairs: Day3.raw_parse(filename))
+      end
+
+      def self.from_parsing_with_toggle(filename)
+        new(pairs: Day3.parse_with_toggle(filename))
       end
 
       def initialize(pairs:)
--- spec/day3_spec.rb
+++ spec/day3_spec.rb
@@ -3,18 +3,37 @@
 require "rspec"
 module Aoc2024::Day3
   RSpec.describe Puzzle do
-    context "with test data" do
-      it "solves correctly" do
-        puzzle = Puzzle.from("spec/data/day03_test.txt")
-        expect(puzzle.solution).to eq(161)
+    context "part one" do
+      context "with test data" do
+        it "solves correctly" do
+          puzzle = Puzzle.from_raw_parsing("spec/data/day03_test.txt")
+          expect(puzzle.solution).to eq(161)
+        end
+      end
+
+      context "with real data" do
+        it "solves correctly" do
+          puzzle = Puzzle.from_raw_parsing("spec/data/day03.txt")
+          expect(puzzle.solution).to eq(182619815)
+        end
       end
     end
 
-    context "with real data" do
-      it "solves correctly" do
-        puzzle = Puzzle.from("spec/data/day03.txt")
-        expect(puzzle.solution).to eq(182619815)
+    context "part two" do
+      context "with test data" do
+        it "solves correctly" do
+          puzzle = Puzzle.from_parsing_with_toggle("spec/data/day03_test_p2.txt")
+          expect(puzzle.solution).to eq(48)
+        end
+      end
+
+      context "with real data" do
+        it "solves correctly" do
+          puzzle = Puzzle.from_parsing_with_toggle("spec/data/day03.txt")
+          expect(puzzle.solution).to eq(80747545)
+        end
       end
     end
   end
 end
+
